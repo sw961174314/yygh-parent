47c224359e0d79727b487bfad41c8ba75a90561e
sw961174314 <961174314@qq.com>
2022-09-29 16:07:00 +0800

    尚医通后端

---
+++ common/service-util/src/main/java/com/sw961174314/common/config/MyBatisPlusConfig.java
@@ -0,0 +1,33 @@
+package com.sw961174314.common.config;
+
+import com.baomidou.mybatisplus.extension.plugins.OptimisticLockerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@EnableTransactionManagement
+@Configuration
+@MapperScan("com.sw961174314.*.mapper")
+public class MyBatisPlusConfig {
+
+    /**
+     * 分页插件
+     * @return
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+        return paginationInterceptor;
+    }
+
+    /**
+     * 乐观锁配置
+     * @return
+     */
+    @Bean
+    public OptimisticLockerInterceptor optimisticLockerInterceptor() {
+        return new OptimisticLockerInterceptor();
+    }
+}
---
+++ model/src/main/java/com/sw961174314/vo/acl/AssignVo.java
@@ -0,0 +1,11 @@
+package com.sw961174314.vo.acl;
+
+import lombok.Data;
+
+@Data
+public class AssignVo {
+
+    private Long roleId;
+
+    private Long[] permissionId;
+}
